a45adcc3a95ce806a899030fc4f2442f67b40b0f
kshamaaditilethakula <114813685+kshamaaditilethakula@users.noreply.github.com>
2024-07-19 21:31:39 -0400

    final

---
+++ nft-collection-creator/fleek.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@fleek-platform/cli').FleekConfig} */
+module.exports = {
+  "sites": [
+    {
+      "slug": "substantial-ocean-thundering",
+      "distDir": "build",
+      "buildCommand": "npm run build"
+    }
+  ]
+};
